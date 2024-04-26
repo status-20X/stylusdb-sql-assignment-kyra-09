d1b07e5dafd20848dbd63e6b19d9e0fecc3d31dc
kyra_09 <aaradhyasinghgaur@gmail.com>
2024-04-26 18:03:05 +0530

    new

--- src/index.js
+++ src/index.js
@@ -3,28 +3,41 @@
 const parseQuery = require('./queryParser');
 const readCSV = require('./csvReader');
 
+// Helper functions for different JOIN types
+function performInnerJoin(/* parameters */) {
+    // Logic for INNER JOIN
+    // ...
+}
+
+function performLeftJoin(/* parameters */) {
+    // Logic for LEFT JOIN
+    // ...
+}
+
+function performRightJoin(/* parameters */) {
+    // Logic for RIGHT JOIN
+    // ...
+}
+
 async function executeSELECTQuery(query) {
-    const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
+    const { fields, table, whereClauses, joinType, joinTable, joinCondition } = parseQuery(query);
     let data = await readCSV(`${table}.csv`);
 
-    // Perform INNER JOIN if specified
+    // Logic for applying JOINs
     if (joinTable && joinCondition) {
         const joinData = await readCSV(`${joinTable}.csv`);
-        data = data.flatMap(mainRow => {
-            return joinData
-                .filter(joinRow => {
-                    const mainValue = mainRow[joinCondition.left.split('.')[1]];
-                    const joinValue = joinRow[joinCondition.right.split('.')[1]];
-                    return mainValue === joinValue;
-                })
-                .map(joinRow => {
-                    return fields.reduce((acc, field) => {
-                        const [tableName, fieldName] = field.split('.');
-                        acc[field] = tableName === table ? mainRow[fieldName] : joinRow[fieldName];
-                        return acc;
-                    }, {});
-                });
-        });
+        switch (joinType.toUpperCase()) {
+            case 'INNER':
+                data = performInnerJoin(data, joinData, joinCondition, fields, table);
+                break;
+            case 'LEFT':
+                data = performLeftJoin(data, joinData, joinCondition, fields, table);
+                break;
+            case 'RIGHT':
+                data = performRightJoin(data, joinData, joinCondition, fields, table);
+                break;
+            // Handle default case or unsupported JOIN types
+        }
     }
 
     const filteredData = whereClauses.length > 0
