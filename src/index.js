92091b6e595bc3dd3e05e215bcc4729eeeafc544
kyra_09 <aaradhyasinghgaur@gmail.com>
2024-04-26 17:35:07 +0530

    new

--- src/index.js
+++ src/index.js
@@ -36,8 +36,9 @@ const filteredData = whereClauses.length > 0
         fields.forEach(field => {
             // Assuming 'field' is just the column name without table prefix
             selectedRow[field] = row[field];
-        } );})
-        return selectedRow;
+        } );
+        return selectedRow; })
+        
 }
 
 module.exports = executeSELECTQuery;
